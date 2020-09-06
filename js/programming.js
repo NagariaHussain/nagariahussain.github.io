d4c9e8185cc9cc2e9cfad17afa0e2a712b60a0a3
hussain nagaria <hussainbhaitech@gmail.com>
2020-09-06 12:12:31 +0530

    Start food page and add some more info to report

--- js/programming.js
+++ js/programming.js
@@ -1,14 +1,8 @@
-var elem = document.querySelector('.main-carousel');
-var flkty = new Flickity(elem, {
+let elem = document.querySelector('.main-carousel');
+let flkty = new Flickity(elem, {
     // options
     cellAlign: 'left',
     contain: true,
     freeScroll: false
         // setGallerySize: false
-});
-
-// element argument can be a selector string
-//   for an individual element
-var flkty = new Flickity('.main-carousel', {
-    // options
 });
